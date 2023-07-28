655c10d718dd8b024e321a64514ddc18b2ffbaa3
prashulkale <97334196+prashulkale@users.noreply.github.com>
2023-07-28 14:35:53 +0530

    .

--- script.js
+++ script.js
@@ -55,10 +55,10 @@ var tl = gsap.timeline({
     }
 })
 
-$('.heading').textillate({ in: { effect: 'rollIn' } });
+// $('.heading').textillate({ in: { effect: 'rollIn' } });
 
  gsap.from(".heading" ,{
-   x : 20
+   y : 90
    
 
  })
@@ -101,9 +101,61 @@ var hoverImgTextDiv = document.querySelector(".page4 .elem   .text-d ")
 var hoverImgText = document.querySelector(".page4 .elem   .text-d h1")
 var hoverImg = document.querySelectorAll(".page4 .elem   img")
 
- main.addEventListener("mousemove" , (dts) =>{
-  crsr.style.left = dts.x + "px"
-  crsr.style.top = dts.y + "px"
- })
+//  main.addEventListener("mousemove" , (dts) =>{
+//   crsr.style.left = dts.clientX + "px"
+//   crsr.style.top = dts.clientY + "px"
+//  })
+
+ window.onmousemove = function(e){
+  crsr.style.top = e.clientY + 'px';
+  crsr.style.left = e.clientX + 'px';
 
 
+} 
+
+
+function page4(){
+  var imgArr  = [
+    "https://plus.unsplash.com/premium_photo-1690164161383-f5ff30a790bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=600&q=60", 
+    "https://images.unsplash.com/photo-1690441426989-0cf9f866a6f4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1661956602139-ec64991b8b16?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1552391744-77952f0cd6c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1576289412237-698ae5427f27?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=600&q=60"
+  
+  ]
+var yeardsc = document.querySelectorAll(".yeardsc")
+var crsr = document.querySelector(".curser");
+
+yeardsc.forEach((event ,i) =>{
+  event.onmousemove = function(e){
+ 
+  crsr.style.width = "25vw";
+  crsr.style.height = "30vh";
+  crsr.style.borderRadius = "0"
+  crsr.style.backgroundImage = `url(${imgArr[i]})`;
+  crsr.classList.add("mixblend") 
+  
+  
+
+
+
+    
+  
+  }
+  event.onmouseleave = function(e){
+ 
+    crsr.style.width = "2vw";
+    crsr.style.height = "2vw";
+    // crsr.style.borderRadius = "0"
+    crsr.style.borderRadius = "50%"
+    // crsr.style.background = `none`;
+  crsr.style.backgroundImage = `url()`;
+
+    crsr.classList.remove("mixblend") 
+    }
+ 
+})
+
+
+}
+page4()
